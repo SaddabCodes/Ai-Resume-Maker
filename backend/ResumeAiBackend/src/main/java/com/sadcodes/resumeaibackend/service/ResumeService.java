41eb0a508e8f7a0e0005d3780cebf5474fb01cad
saddab <saddabakhtar17@gmail.com>
2025-11-28 22:47:28 +0530

    update ResumeService class

--- backend/ResumeAiBackend/src/main/java/com/sadcodes/resumeaibackend/service/ResumeService.java
+++ backend/ResumeAiBackend/src/main/java/com/sadcodes/resumeaibackend/service/ResumeService.java
@@ -1,6 +1,8 @@
 package com.sadcodes.resumeaibackend.service;
 
+import java.io.IOException;
+
 public interface ResumeService {
 
-    String generateResumeService(String userResumeDescription);
+    String generateResumeService(String userResumeDescription) throws IOException;
 }
--- backend/ResumeAiBackend/src/main/java/com/sadcodes/resumeaibackend/service/ResumeServiceImpl.java
+++ backend/ResumeAiBackend/src/main/java/com/sadcodes/resumeaibackend/service/ResumeServiceImpl.java
@@ -1,8 +1,15 @@
 package com.sadcodes.resumeaibackend.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.core.io.ClassPathResource;
 
-public class ResumeServiceImpl implements ResumeService{
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class ResumeServiceImpl implements ResumeService {
 
     private final ChatClient chatClient;
 
@@ -11,7 +18,29 @@ public class ResumeServiceImpl implements ResumeService{
     }
 
     @Override
-    public String generateResumeService(String userResumeDescription) {
-        return "";
+    public String generateResumeService(String userResumeDescription) throws IOException {
+
+        String promptString = this.loadPromptFromFile("resume_prompt,txt");
+        String  promptContent = this.putValuesToTemplate(promptString,Map.of("userDescription",userResumeDescription));
+
+        Prompt prompt = new Prompt(promptContent);
+
+        String response = chatClient.prompt(prompt).call().content();
+        return response;
     }
+
+   public String loadPromptFromFile(String fileName) throws IOException {
+
+
+
+        Path path = new ClassPathResource(fileName).getFile().toPath();
+        return Files.readString(path);
+    }
+
+   public String putValuesToTemplate(String template, Map<String ,String> values){
+        for (Map.Entry<String ,String > entry: values.entrySet())
+        {
+            template.replace("{" + entry.getKey()+ "}",entry.getValue());
+        }
+   }
 }
